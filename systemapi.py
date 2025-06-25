18df1163ffe105cefb39e9e51115010c33ca6a1b
Atul Joshi <joshiaatul24@gmail.com>
2025-06-25 20:27:18 +0530

    shlex_quote

--- systemapi.py
+++ systemapi.py
@@ -1,4 +1,9 @@
 import os 
+import shlex
+
 user_input = input("Enter a parameter: ")
 command = f"echo Hellow {user_input}"
-os.system(command)
+safe_command = shlex.quote(command)
+print(safe_command)
+os.system(safe_command)
+#os.system(command)
